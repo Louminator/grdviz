c3d0ceedb34eaffbe961bf010646498cd1f22bc7
Louis Rossi <rossi@math.udel.edu>
2013-08-29 15:09:40 -0400

    Just starting out...

---
+++ grd_explore.py
@@ -0,0 +1,211 @@
+from PyQt4 import QtCore, QtGui
+from PyQt4.QtGui import QWidget
+
+import sys
+
+from mpltest import Ui_Form
+from matplotlibwidget import *
+
+from scipy import *
+from string import split,atof,atoi
+
+from vtx import *
+from event import Event
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    _fromUtf8 = lambda s: s
+
+class Plot_Widget(QWidget,Ui_Form):
+    def __init__(self, data2plot=None, parent = None):
+        self.grddata = {}
+        self.MaxThreads = 4
+        self.NMesh = 75
+        self.FrameThreads = []
+        self.FrameQueue =  []
+
+        QWidget.__init__(self)
+        super(Plot_Widget, self).__init__(parent)
+        self.setupUi(self)
+#        self.mplwidget = MatplotlibWidget(self, title='Example',
+#                                          xlabel='Linear scale',
+#                                          ylabel='Log scale',
+#                                          hold=True, yscale='log')
+#        self.mplwidget.setFocus()
+#        self.setCentralWidget(self.mplwidget)
+
+        try:
+            f = open('/home/rossi/Research/Oseen-explorations/expB-lowres/egrid.default','r')
+            txt = f.read()
+            f.close()
+            nums = split(txt)
+            self.num = map(atof,nums[0:4])
+            self.gridn = atoi(nums[4])
+
+            self.xMin.setValue(self.num[0])
+            self.xMax.setValue(self.num[2])
+            self.yMin.setValue(self.num[1])
+            self.yMax.setValue(self.num[3])
+            
+            self.xMin.setSingleStep((self.num[2]-self.num[0])/(self.gridn-1.))
+            self.yMin.setSingleStep((self.num[3]-self.num[1])/(self.gridn-1.))
+            self.xMax.setSingleStep((self.num[2]-self.num[0])/(self.gridn-1.))
+            self.yMax.setSingleStep((self.num[3]-self.num[1])/(self.gridn-1.))
+        except IOError:
+            print "No egrid.default file found."
+            
+        self.plot(self.mplwidget.axes)
+#        QtCore.QObject.connect(self.timeDial, QtCore.SIGNAL(_fromUtf8("valueChanged(int)")), self.newplot)
+        self.mplwidget.leaveEvent = self.mplleaveEvent
+        self.mplwidget.enterEvent = self.mplenterEvent
+        self.mplwidget.wheelEvent = self.mplwheelEvent
+        
+        self.timer = QtCore.QTimer()
+        QtCore.QObject.connect(self.timer, QtCore.SIGNAL("timeout()"), self.playUpdate)
+                
+    def read_grd(self,n):
+        name = '/home/rossi/Research/Oseen-explorations/expB-lowres/expB'
+        name = '/home/rossi/Research/Oseen-explorations/lamb-dipole-perturb-A'
+        grdname = name+'{0:04d}'.format(n)+'.grd'
+        try:
+            f = open(grdname,'r')
+            txt = f.read()
+            f.close()
+            ws = split(txt)
+            w = map(atof,ws)
+            w = reshape(w,(self.gridn,self.gridn))
+        except IOError:
+            print "No grd file found."
+        return(w)
+        
+    def quit_gui(self):
+        self.close()
+        
+    def play(self):
+        print self.Play.isChecked()
+        self.timer.start(200)
+            
+    def pause(self):
+        self.timer.stop()
+            
+    def playUpdate(self):
+        if self.Play.isChecked():
+            if (self.CurrentFrame.value()>100):
+                self.CurrentFrame.setValue(99)
+                self.timer.stop()
+                self.Pause.setChecked(True)
+            self.CurrentFrame.setValue(self.CurrentFrame.value()+1)
+            self.newplot()
+        if self.Rewind.isChecked():
+            if (self.CurrentFrame.value() == 0):
+                self.CurrentFrame.setValue(1)
+                self.timer.stop()
+                self.Pause.setChecked(True)
+            self.CurrentFrame.setValue(self.CurrentFrame.value()-1)
+            self.newplot()            
+
+    def mplwheelEvent(self,event):
+        print "Weeee"
+        print event.delta()
+        
+    def mplenterEvent(self,event):
+        print "Welcome"
+        
+    def mplleaveEvent(self,event):
+        print "B-Bye"
+
+    def FrameDone(self,n):
+        if (n == self.CurrentFrame.value()):
+            self.newplot()
+        self.FrameThreads.remove(n)
+        if (len(self.FrameQueue) > 0):
+            self.grddata[self.FrameQueue[0]] = Vorticity_Frame(self.FrameQueue[0])
+            self.FrameThreads.append(self.FrameQueue[0])
+            self.grddata[self.FrameQueue[0]].SpawnMesh(self.NMesh,self.FrameDone)
+            self.FrameQueue.remove(self.FrameQueue[0])
+
+    def newplot(self):
+        a=self.CurrentFrame.value()
+        
+        print self.FrameThreads
+        print self.FrameQueue
+        
+        if  (not a in self.grddata):
+            self.grddata[a] = Vorticity_Frame(a)
+            
+        if (self.grddata[a].MeshReady() == 0):
+            if (len(self.FrameThreads) < self.MaxThreads):
+                self.grddata[a] = Vorticity_Frame(a)
+                self.FrameThreads.append(a)
+                self.grddata[a].SpawnMesh(self.NMesh,self.FrameDone)
+            else:
+                if (not a in self.FrameQueue):
+                    if (self.Pause.isChecked()):
+                        self.FrameQueue.insert(0,a)
+                    else:
+                        self.FrameQueue.append(a)
+        
+        if (self.grddata[a].MeshReady() == 2):
+            self.grddata[a].GrabMesh()
+#            if (a in self.FrameThreads):
+#                self.FrameThreads.remove(a)
+            self.mplwidget.axes.cla()
+            self.mplwidget.axes.pcolormesh(self.grddata[a].xm,self.grddata[a].ym,self.grddata[a].wm,edgecolors='None',shading='None',rasterized=True)
+            self.mplwidget.axes.set_xlim((self.xMin.value(),self.xMax.value()))
+            self.mplwidget.axes.set_ylim((self.yMin.value(),self.yMax.value()))
+            self.mplwidget.draw()
+
+        else:
+            self.mplwidget.axes.cla()
+            self.mplwidget.axes.set_xlim((self.xMin.value(),self.xMax.value()))
+            self.mplwidget.axes.set_ylim((self.yMin.value(),self.yMax.value()))
+            xmid = (self.xMin.value()+self.xMax.value())/2.
+            ymid = (self.yMin.value()+self.yMax.value())/2.
+            self.mplwidget.axes.text(xmid,ymid,'Working...',fontsize=18, \
+                horizontalalignment='center',verticalalignment='center',color='red')
+            self.mplwidget.draw()
+
+    def newplot3(self):
+        a=self.CurrentFrame.value()
+        
+        if a in self.grddata:
+            meshdata = self.grddata[a]
+        else:
+            meshdata = self.read_grd(a)
+            self.grddata[a] = meshdata
+            
+        x = linspace(self.num[0],self.num[2],self.gridn)
+        y = linspace(self.num[1],self.num[3],self.gridn)
+        
+#        meshdata = meshdata[::2,::2]
+#        x = x[::2]
+#        y = y[::2]
+        X,Y = meshgrid(x, y)
+        
+#        self.mplwidget.axes.pcolor(X,Y,meshdata,edgecolors='None',shading='faceted',rasterized=True)
+        self.mplwidget.axes.pcolormesh(X,Y,meshdata,edgecolors='None',shading='None',rasterized=True)
+        self.mplwidget.axes.set_xlim((self.xMin.value(),self.xMax.value()))
+        self.mplwidget.axes.set_ylim((self.yMin.value(),self.yMax.value()))
+        
+        xmid = (x[0]+x[-1])/2.
+        ymid = (x[0]+x[-1])/2.
+        self.mplwidget.axes.text(xmid,ymid,'Working...',fontsize=18, \
+            horizontalalignment='center',verticalalignment='center',color='red')
+        
+#        self.mplwidget.axes.contour(X,Y,meshdata,rasterized=True)
+#        self.mplwidget.axes.colorbar()
+        self.mplwidget.draw()
+
+    def plot(self, axes):
+        self.newplot()
+
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    Form = QtGui.QWidget()
+    plot = Plot_Widget()
+    plot.show()
+
+    sys.exit(app.exec_())
+
---
+++ vtx.py
@@ -0,0 +1,173 @@
+# -*- coding: utf-8 -*-
+"""
+Class definition for vorticity data.
+
+Created on Fri Aug  2 20:46:35 2013
+
+@author: rossi
+"""
+
+from scipy import *
+from string import split,atof
+#import matplotlib.pylab as plt
+from PyQt4 import QtCore, QtGui
+
+from multiprocessing import Process,Pipe
+
+
+# Read in from the file.
+
+n = 1
+
+class Vorticity_Frame():
+    
+    def __init__(self,n):
+        
+        self.FrameNumber = n
+
+        self.x0 = -2.
+        self.y0 = -2.
+
+        self.x1 = 2.
+        self.y1 = 2.
+        
+        # GridStatus: 0=no grid, 1=working on grid, 2=grid created
+        # GridStatus: -1 = no data at all.
+        self.GridStatus = 0
+
+        self.timer = QtCore.QTimer()
+        QtCore.QObject.connect(self.timer, QtCore.SIGNAL("timeout()"), self.MeshReady)
+        
+        name = '/home/rossi/Research/Oseen-explorations/lamb-dipole-perturb-B/lamb-perturb-B'
+        vtxname = name+'{0:04d}'.format(n)+'.vtx'
+        try:
+            f = open(vtxname,'r')
+            txt = f.read()
+            f.close()
+            vs = split(txt)
+            vdata = map(atof,vs)
+            self.vdata = reshape(vdata,(len(vdata)/6,6))
+            self.num_vorts = len(vdata)
+        except IOError:
+            print "No vtx file found."
+            self.GridStatus = -1
+
+# mesh it.
+
+    def mesh(self,nmesh):
+
+#        nmesh = 200
+
+        x = r_[self.x0:self.x1:nmesh*1j]
+        y = r_[self.y0:self.y1:nmesh*1j]
+
+        [self.xm,self.ym] = meshgrid(x,y)
+
+        xtmp = self.xm.reshape(size(self.xm),)
+        ytmp = self.ym.reshape(size(self.ym),)
+
+        w = zeros(size(self.xm))
+
+        for k in range(len(self.vdata)):
+            dx = xtmp-self.vdata[k,0]
+            dy = ytmp-self.vdata[k,1]
+            c = cos(self.vdata[k,5])
+            s = sin(self.vdata[k,5])
+            r2 = dx**2 + dy**2
+            idx = (r2/self.vdata[k,3]>1.0e-10)
+            
+            tmp = -(dx[idx]**2/(c**2/self.vdata[k,4] + s**2/self.vdata[k,4])+ \
+            dy[idx]**2/(s**2/self.vdata[k,4] + c**2/self.vdata[k,4]) + \
+            2.*dx[idx]*dy[idx]*c*s*(1./self.vdata[k,4]+self.vdata[k,4]))/4./self.vdata[k,3]        
+            w[idx] += self.vdata[k,2]*exp(tmp)/2./self.vdata[k,3]
+    
+        print 'done'
+        self.wm = w.reshape((len(x),len(y)))
+
+    def meshthread(self,nmesh,conn):
+
+        x = r_[self.x0:self.x1:nmesh*1j]
+        y = r_[self.y0:self.y1:nmesh*1j]
+
+        [xm,ym] = meshgrid(x,y)
+
+        xtmp = xm.reshape(size(xm),)
+        ytmp = ym.reshape(size(ym),)
+
+        w = zeros(size(xm))
+
+        for k in range(len(self.vdata)):
+            dx = xtmp-self.vdata[k,0]
+            dy = ytmp-self.vdata[k,1]
+            c = cos(self.vdata[k,5])
+            s = sin(self.vdata[k,5])
+            r2 = dx**2 + dy**2
+            idx = (r2/self.vdata[k,3]>1.0e-10)
+            
+            tmp = -(dx[idx]**2/(c**2/self.vdata[k,4] + s**2/self.vdata[k,4])+ \
+            dy[idx]**2/(s**2/self.vdata[k,4] + c**2/self.vdata[k,4]) + \
+            2.*dx[idx]*dy[idx]*c*s*(1./self.vdata[k,4]+self.vdata[k,4]))/4./self.vdata[k,3]        
+            w[idx] += self.vdata[k,2]*exp(tmp)/2./self.vdata[k,3]
+    
+        wm = w.reshape((len(x),len(y)))
+        conn.send((xm,ym,wm))
+
+    def SpawnMesh(self,nmesh,AlertFcn):
+        self.parent_conn,self.child_conn = Pipe()
+        self.proc = Process(target=self.meshthread,args=(nmesh,self.child_conn))
+        self.proc.start()
+        self.timer.start(1000)
+        self.GridStatus = 1
+        self.alert = AlertFcn
+
+        
+    def GrabMesh(self):
+        if (self.parent_conn.poll()):
+            self.xm,self.ym,self.wm = self.parent_conn.recv()
+            self.GridStatus=2
+
+    def MeshReady(self):
+        if (self.GridStatus == 1):
+            if (self.parent_conn.poll()):
+                self.GrabMesh()
+                self.timer.stop()
+                self.alert(self.FrameNumber)
+        return(self.GridStatus)
+            
+bigv = {}
+
+parent_conn = {}
+child_conn = {}
+p = {}
+
+for k in range(4):
+    bigv[k] = Vorticity_Frame(k)
+#    parent_conn[k],child_conn[k] = Pipe()
+#    p[k] = Process(target=bigv[k].meshthread,args=(100,child_conn[k],))
+#    p[k].start()
+
+#working = True
+#while (working):
+#    working = False
+#    for k in range(4):
+#        print working
+#        print (k,parent_conn[k].poll())
+#        if (not parent_conn[k].poll()):
+#            working = True
+#    raw_input()
+#print 'Everyone is done.'
+#
+#for k in range(4):
+#    print child_conn[k].poll()
+#    bigv[k].xm,bigv[k].ym,bigv[k].wm = parent_conn[k].recv()
+#    print child_conn[k].poll()
+
+#for k in range(4):
+#    p[k].join()
+#    print p[k].is_alive()
+
+#bigv[0].mesh(200)
+
+#plt.ion()
+#plt.pcolormesh(bigv[0].xm,bigv[0].ym,bigv[0].wm)
+#plt.show()
